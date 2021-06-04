8954f5160c51b3f0e3a25b49cdbdb1cba66e9a05
Francesco <francescococlite@gmail.com>
2021-06-04 18:09:09 +0200

    inizio funzione annulla

--- js/script.js
+++ js/script.js
@@ -2,13 +2,32 @@ var submit = document.getElementById('btn-genera');
 submit.addEventListener("click", function(){
 
     var nome = document.getElementById('nome').value;
-    console.log(nome);
-
-    var km = document.getElementById('km').value;
-    console.log(km);
+    document.getElementById('nome-biglietto').innerHTML = nome;
 
     var eta = document.getElementById('eta').value;
-    console.log(eta);
+    document.getElementById('offerta').innerHTML = eta;
 
+    var km = document.getElementById('km').value;
+    document.getElementById('costo').innerHTML = km * 0.21 + 'Euro';
+    if (eta == 'over65'){
+        document.getElementById('costo').innerHTML = km * 0.21 * 0.6 + 'Euro';
+
+    }else if (eta == 'minorenne'){
+        document.getElementById('costo').innerHTML = km * 0.21 * 0.8 + 'Euro';
+    }
+
+    var ncarrozza = Math.floor((Math.random() * 9) + 1);
+    document.getElementById('carrozza').innerHTML = ncarrozza;
+
+    var cp = Math.floor((Math.random() * 90000) + 10000);
+    document.getElementById('codice-cp').innerHTML = cp;
 })
+var submit = document.getElementById('btn-annulla');
+submit.addEventListener("click", function(){
+
+
+    
+
+
+)}
     
