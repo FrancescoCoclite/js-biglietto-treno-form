806316c6b9dbce0f0a5076d1d9df718147af1b65
Francesco <francescococlite@gmail.com>
2021-06-04 17:08:23 +0200

    inizio funzione

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var submit = document.getElementById('btn-genera');
+submit.addEventListener("click", function(){
+
+    var nome = document.getElementById('nome').value;
+    console.log(nome);
+
+    var km = document.getElementById('km').value;
+    console.log(km);
+
+    var eta = document.getElementById('eta').value;
+    console.log(eta);
+
+})
+    
